01b62229267926692ee856481fed349571b3e7b4
maheshwaran-p <mpmahesh1022@gmail.com>
2021-05-21 09:43:00 -0700

    first commit

---
+++ app/src/main/assets/project.js
@@ -0,0 +1,140 @@
+// JSON file describing the AR-experience for offline usage
+var projectJSONOffline =
+{
+    "isCloud": false,
+    "tcId": "60a604115a56097d830dd62a",
+    "name": "testing app",
+    "targets": [
+        {
+            "name": "Screenshot 2021-05-20 120931",
+            "augmentations": [
+                {
+                    "height": 0.1,
+                    "type": "Label",
+                    "targetId": "60a6043f5a56097d830dd62b",
+                    "minSDK": "6.1.0",
+                    "modDat": 1621493359597,
+                    "creDat": 1621493348792,
+                    "properties": {
+                        "name": "label_1",
+                        "text": "Your label",
+                        "style": {
+                            "backgroundColor": "rgba(245,236,236,1)",
+                            "fontStyle": "italic",
+                            "textColor": "rgba(39,60,242,1)"
+                        },
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 1,
+                            "y": 1,
+                            "z": 1
+                        },
+                        "translate": {
+                            "x": -0.5472776116484807,
+                            "y": 0.4509972617306471,
+                            "z": 2.2032107499922474e-7
+                        },
+                        "zOrder": 0
+                    },
+                    "fileSize": 0,
+                    "id": "60a6066445c92c7d8b8aa9e2"
+                },
+                {
+                    "type": "Button",
+                    "height": 0.3,
+                    "uri": "augmentation-assets/1628101416_Screenshot_2021_05_20_120931/60a6096845c92c7d8b8aa9e6/image.png",
+                    "targetId": "60a6043f5a56097d830dd62b",
+                    "minSDK": "6.1.0",
+                    "modDat": 1621494127145,
+                    "creDat": 1621494120589,
+                    "properties": {
+                        "clickUrl": "https://www.facebook.com/https://www.facebook.com/",
+                        "name": "button_1",
+                        "forceNativeBrowser": "false",
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 1,
+                            "y": 1,
+                            "z": 1
+                        },
+                        "translate": {
+                            "x": -0.5523947673446169,
+                            "y": 0.15219039650925334,
+                            "z": 0
+                        },
+                        "zOrder": 1
+                    },
+                    "fileSize": 3967,
+                    "id": "60a6096845c92c7d8b8aa9e6"
+                }
+            ]
+        }
+    ],
+    "type": "ImageTargetCollection",
+    "wtcFiles": [
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "6.0",
+            "creDat": 1621493258851
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "6.1",
+            "creDat": 1621493258851
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "6.2",
+            "creDat": 1621493258851
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "7.0",
+            "creDat": 1621493258851
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "7.1",
+            "creDat": 1621493258851
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "7.2",
+            "creDat": 1621493258851
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "8.0",
+            "creDat": 1621493258851
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "latest",
+            "creDat": 1621493258851
+        }
+    ],
+    "settings": {
+        "tracker": {}
+    }
+}
+;
